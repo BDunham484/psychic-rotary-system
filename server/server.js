760e847465785c2d64bfe39aeccd94cbb363e1ec
ThereWolf <bdunham484@gmail.com>
2022-10-18 09:30:42 -0500

    add ApolloServer

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 //import express.js package
 const express = require('express');
+//import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 //import database connection to MongoDB via Mongoose
 const db = require('./config/connection');
+const { type } = require('os');
 
 //set environment variable
 const PORT = process.env.PORT || 3001;
+//create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 //instantiate the server
 const app = express();
 
@@ -13,9 +23,21 @@ app.use(express.urlencoded({ extended: false }));
 //parse incoming JSON data
 app.use(express.json());
 
-//establish connection to server via the listen() method
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
+//create a new instance of an Apollo server with the GraphQl schema
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    //integrate our Apollo server with the Express application as middleware
+    server.applyMiddleware({ app });
+
+    //establish connection to server via the listen() method
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+        });
     });
-});
+};
+
+//call the async function to start the server
+startApolloServer(typeDefs, resolvers);
+
