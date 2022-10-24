015283b300963061b52b40dfbaa8960a75c86d23
ThereWolf <bdunham484@gmail.com>
2022-10-24 16:34:00 -0500

    mapped concerts

--- client/src/components/TodaysConcerts/index.js
+++ client/src/components/TodaysConcerts/index.js
@@ -1,19 +1,25 @@
-const ConcertList = ({ concerts, dateTime }) => {
+const ConcertList = ({ concerts}) => {
     if (!concerts.length) {
         return <h3>No Concerts Today</h3>;
     }
-
+    console.log('concerts!!!!!!!');
+    console.log(concerts);
     return (
         <div>
-            <h3>{dateTime}</h3>
-            {concerts &&
-                concerts.map(concert => (
-                    <div key={concert._id}>
-                        <p>{concert.artists}</p>
-                        <p>{concert.description}</p>
-                        <p>{concert.venue}</p>
-                    </div>
-                ))}
+            {/* <h3 className="todays-date">{date}</h3> */}
+            <div>
+                {concerts &&
+                    concerts.map(concert => (
+                        <div key={concert._id}  className="events">
+                            {/* <p>{concert.dateTime}</p> */}
+                            <p>{concert.artists}</p>
+                            <p>{concert.description}</p>
+                            <p>{concert.venue}</p>
+                        </div>
+                    ))}
+            </div>
         </div>
     )
 }
+
+export default ConcertList;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,26 @@
 import { useQuery } from '@apollo/client';
 import { GET_TODAYS_CONCERTS } from '../utils/queries';
+import TodaysConcerts from '../components/TodaysConcerts';
 
 const Home = () => {
     //use useQuery hook to make query request
     const { loading, data } = useQuery(GET_TODAYS_CONCERTS);
 
     const concerts = data?.concerts || [];
-    console.log(concerts)
+    console.log('HOME CONCERTS!!!!!!!!!');
+    // console.log(concerts[1].dateTime)
+    const date = concerts[1].dateTime;
+    console.log(date);
 
     return (
         <div className="page-wrapper">
-            <h1>HOME PAGE</h1>
+            <h1 className='todays-date'>{date}</h1>
             <div>
-
+                {loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <TodaysConcerts concerts={concerts}/>
+                )}
             </div>
         </div>
     )
