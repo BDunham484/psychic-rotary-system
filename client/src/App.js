bf3ce99a2f9260dea7a1c87cb090d724f2219c06
ThereWolf <bdunham484@gmail.com>
2022-10-24 15:25:37 -0500

    add Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+//establish link to GraphQL server
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+//use ApolloClient() constructor to instantiate the Apollo Client instance/create connection to API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
 function App() {
 
   return (
+    <ApolloProvider client={client}>
       <div>
         <Header></Header>
-        <main> 
+        <main>
           <Home></Home>
         </main>
         <Footer></Footer>
-      </div> 
+      </div>
+    </ApolloProvider>
+
   );
 }
 
