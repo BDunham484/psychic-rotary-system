42b3e246d201797071ee273975e666e0e3faf6dd
ThereWolf <bdunham484@gmail.com>
2022-11-16 10:50:27 -0600

    add signup functionality/client

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,13 +1,66 @@
 import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
+import Auth from '../utils/auth';
 
 const Signup = () => {
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
 
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            //execute addUser mutation and pass in variable data from form
+            const { data } = await addUser({
+                variables: { ...formState }
+            });
+            console.log(data);
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     return (
         <div className='page-wrapper'>
-            Signup
+            <form onSubmit={handleFormSubmit}>
+                <input
+                placeholder='Your username'
+                name='username'
+                type='username'
+                id='username'
+                value={formState.username}
+                onChange={handleChange}
+                />
+                <input
+                placeholder='Your email'
+                name='email'
+                type='email'
+                id='email'
+                value={formState.email}
+                onChange={handleChange}
+                />
+                <input 
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+                value={formState.password}
+                onChange={handleChange}
+                />
+                <button type='submit'>Submit</button>
+            </form>
+            {error && <div>Sign up failed</div>}
         </div>
     )
 }
