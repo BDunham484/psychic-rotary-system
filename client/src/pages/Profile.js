9eb2a935ed3c0a2f6f0e8c27ac5a7b17902ba66c
ThereWolf <bdunham484@gmail.com>
2022-11-16 09:53:07 -0600

    add profile query

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,24 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
 const Profile = () => {
+    const { username: userParam } = useParams();
+
+    const { loading, data } = useQuery(QUERY_USER, {
+        variables: { username: userParam}
+    });
+
+    const user = data?.user || {};
+    console.log(user);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div className='page-wrapper'>
-            Profile
+            <h2>Viewing {user.username}'s profile.</h2>
         </div>
     )
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,3 +14,26 @@ export const GET_TODAYS_CONCERTS = gql`
         }
     }
 `;
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            concertCount
+            concerts {
+                _id
+                artists
+                artistsLink
+                description
+                dateTime
+                venue
+                address
+                website
+                email
+                ticketLink
+            }
+        }
+    }
+`;
