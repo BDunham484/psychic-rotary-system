b6c6b45ebe35e949f8b18b258ef3453466e278b4
ThereWolf <bdunham484@gmail.com>
2022-11-18 14:57:59 -0600

    add me queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -54,3 +54,41 @@ export const QUERY_USER = gql`
         }
     }
 `;
+
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            concertCount
+            concerts {
+                _id
+                artists
+                artistsLink
+                description
+                dateTime
+                venue
+                address
+                website
+                email
+                ticketLink
+            }
+        }
+    }
+`;
+
+export const QUERY_ME_BASIC = gql`
+    {
+        me {
+            _id
+            username
+            email
+            concertCount
+            concerts {
+                _id
+                artists
+            }
+        }
+    }
+`;
