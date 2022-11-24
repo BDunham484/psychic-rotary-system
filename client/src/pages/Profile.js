0fb0f31ad4091d7031c2c2606acfcbcd4294875d
ThereWolf <bdunham484@gmail.com>
2022-11-23 19:49:07 -0600

    add user concert data

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,12 +4,11 @@ import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 const Profile = () => {
     const { username: userParam } = useParams();
-
+    //query that checks param value then conditionally runs query based on result
     const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
         variables: { username: userParam}
     });
-    
-
+    //user declaration set up to handle each type of response from above useQuery
     const user = data?.me || data?.user || {};
     console.log(user);
 
@@ -20,6 +19,23 @@ const Profile = () => {
     return (
         <div className='page-wrapper'>
             <h2>Viewing {user.username}'s profile.</h2>
+            <p>Concert Count: {user.concertCount}</p>
+            <div>
+                {user.concerts.map((concert, index) => (
+                    <div key={index} className="events">
+                        <div>Date: {concert.dateTime}</div>
+                        <div>
+                            Artist: {concert.artists}
+                        </div>
+                        <div>Venue: {concert.venue}</div>
+                        <div>Address: {concert.address}</div>
+                        <div>Website: {concert.website}</div>
+                        <div>Email: {concert.eamil}</div>
+                        <div>Ticket Link: {concert.ticketLink}</div>
+                        <div>Artists Link: {concert.artistsLink}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
