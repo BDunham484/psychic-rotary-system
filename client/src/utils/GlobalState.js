3d482672a6374718fbc3e7d4b3330a33ab35f327
ThereWolf <bdunham484@gmail.com>
2023-01-13 11:03:46 -0600

    initiate scraper

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -36,64 +36,64 @@ const ConcertProvider = ({ children }) => {
     //set initial state using today's date
     const [date, setDate] = useState(today);
 
-    // const [scraperDate, setScraperDate] = useState(today);
+    const [scraperDate, setScraperDate] = useState(today);
 
-    // useEffect(() => {
-    //     //  delcare empty array for dates
-    //     const dateArr = [];
-    //     //push todays date into dateArr
-    //     dateArr.push(today);
-    //     //function to get the next day based on the date passed in to it
-    //     const nextDay = (date) => {
-    //         const next = new Date(date);
-    //         next.setDate(next.getDate() + 1);
-    //         const theNextDay = next.toDateString();
-    //         return theNextDay;
-    //     }
-    //     //save date to another variable for for loop
-    //     let arrayDate = today;
-    //     //for loop that continously gets upcoming dates and pushes them to array
-    //     for (let i = 0; i < 89; i++) {
-    //         let nextDate = nextDay(arrayDate);
-    //         dateArr.push(nextDate);
-    //         arrayDate = nextDate;
-    //     }
+    useEffect(() => {
+        //  delcare empty array for dates
+        const dateArr = [];
+        //push todays date into dateArr
+        dateArr.push(today);
+        //function to get the next day based on the date passed in to it
+        const nextDay = (date) => {
+            const next = new Date(date);
+            next.setDate(next.getDate() + 1);
+            const theNextDay = next.toDateString();
+            return theNextDay;
+        }
+        //save date to another variable for for loop
+        let arrayDate = today;
+        //for loop that continously gets upcoming dates and pushes them to array
+        for (let i = 0; i < 89; i++) {
+            let nextDate = nextDay(arrayDate);
+            dateArr.push(nextDate);
+            arrayDate = nextDate;
+        }
 
-    //     let index = 0;
-    //     const delay = (1000 * 20)
+        let index = 0;
+        const delay = (1000 * 20)
         
-    //     let interval = setInterval(function () {
-    //         index += 1;
-    //         if (index >= 90) {
-    //             return () => clearInterval(interval);
-    //         }
-    //         console.log('interval has run: ' + index);
-    //         console.log('DATE TO BE SCRAPED: ' + dateArr[index])
-    //         setScraperDate(dateArr[index]);
-    //     }, delay);
+        let interval = setInterval(function () {
+            index += 1;
+            if (index >= 90) {
+                return () => clearInterval(interval);
+            }
+            console.log('interval has run: ' + index);
+            console.log('DATE TO BE SCRAPED: ' + dateArr[index])
+            setScraperDate(dateArr[index]);
+        }, delay);
 
 
-    // }, [today])
+    }, [today])
 
-    // const { data: concertData } = useQuery(AUSTIN_CONCERT_SCRAPER, {
-    //     // variables: { date: today }
-    //     variables: { date: scraperDate }
-    // })
+    const { data: concertData } = useQuery(AUSTIN_CONCERT_SCRAPER, {
+        // variables: { date: today }
+        variables: { date: scraperDate }
+    })
 
-    // if (concertData) {
-    //     console.log(concertData.austinConcertScraper.length / 2 + ' days of concerts have been scraped.');
-    //     console.log(concertData);
-    // };
+    if (concertData) {
+        console.log(concertData.austinConcertScraper.length / 2 + ' days of concerts have been scraped.');
+        console.log(concertData);
+    };
 
     const [austinScraper, setAustinScraper] = useState([[]]);
 
-    // useEffect(() => {
-    //     if (concertData) {
-    //             const concertDataArr = concertData.austinConcertScraper
-    //             setAustinScraper(concertDataArr)
-    //     }
+    useEffect(() => {
+        if (concertData) {
+                const concertDataArr = concertData.austinConcertScraper
+                setAustinScraper(concertDataArr)
+        }
 
-    // }, [concertData, austinScraper])
+    }, [concertData, austinScraper])
 
 
     return <Provider value={{
