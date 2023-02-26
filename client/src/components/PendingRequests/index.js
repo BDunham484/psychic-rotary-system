5208ac98207a7e7489d63dcd38e250d9800ee4fd
ThereWolf <bdunham484@gmail.com>
2023-02-26 00:01:20 -0600

    add conditional display of sent/received requests

--- client/src/components/PendingRequests/index.js
+++ client/src/components/PendingRequests/index.js
@@ -35,51 +35,51 @@ const PendingRequests = ({ user }) => {
                         </>
                     }
 
-                    {(user.sentCount > 0 && user.receivedCount > 0) &&
-                        requestSwitch ? (
-                        <>
-                            <div className="profile-friends-sub-header">
-                                <div className='friends-sub-titles'>SENT</div>
-                                <Switch
-                                    onChange={handleRequestSwitch}
-                                    checked={requestSwitch}
-                                    offColor={'#525050'}
-                                    onColor={'#525050'}
-                                    offHandleColor={'#383737'}
-                                    onHandleColor={'#383737'}
-                                    uncheckedIcon={false}
-                                    checkedIcon={false}
-                                    boxShadow={'#eee3d0'}
-                                    activeBoxShadow={'#eee3d0'}
-                                />
-                                <div className="unSwitched">RECEIVED</div>
-                            </div>
-                            <SentRequests user={user} />
-                        </>
+                    {!(user.sentCount > 0 && user.receivedCount > 0) ? (
+                        <></>
                     ) : (
-                        <>
-                            <div className="profile-friends-sub-header">
-                                <div className='unSwitched'>SENT</div>
-                                <Switch
-                                    onChange={handleRequestSwitch}
-                                    checked={requestSwitch}
-                                    offColor={'#525050'}
-                                    onColor={'#525050'}
-                                    offHandleColor={'#383737'}
-                                    onHandleColor={'#383737'}
-                                    uncheckedIcon={false}
-                                    checkedIcon={false}
-                                    boxShadow={'#eee3d0'}
-                                    activeBoxShadow={'#eee3d0'}
-                                />
-                                <div className="friends-sub-titles">RECEIVED</div>
-                            </div>
-                            <ReceivedRequests user={user} />
-                        </>
-                    )
-                    }
-
-
+                        requestSwitch ? (
+                            <>
+                                <div className="profile-friends-sub-header">
+                                    <div className='friends-sub-titles'>SENT</div>
+                                    <Switch
+                                        onChange={handleRequestSwitch}
+                                        checked={requestSwitch}
+                                        offColor={'#525050'}
+                                        onColor={'#525050'}
+                                        offHandleColor={'#383737'}
+                                        onHandleColor={'#383737'}
+                                        uncheckedIcon={false}
+                                        checkedIcon={false}
+                                        boxShadow={'#eee3d0'}
+                                        activeBoxShadow={'#eee3d0'}
+                                    />
+                                    <div className="unSwitched">RECEIVED</div>
+                                </div>
+                                <SentRequests user={user} />
+                            </>
+                        ) : (
+                            <>
+                                <div className="profile-friends-sub-header">
+                                    <div className='unSwitched'>SENT</div>
+                                    <Switch
+                                        onChange={handleRequestSwitch}
+                                        checked={requestSwitch}
+                                        offColor={'#525050'}
+                                        onColor={'#525050'}
+                                        offHandleColor={'#383737'}
+                                        onHandleColor={'#383737'}
+                                        uncheckedIcon={false}
+                                        checkedIcon={false}
+                                        boxShadow={'#eee3d0'}
+                                        activeBoxShadow={'#eee3d0'}
+                                    />
+                                    <div className="friends-sub-titles">RECEIVED</div>
+                                </div>
+                                <ReceivedRequests user={user} />
+                            </>
+                        )
+                    )}
                 </>
             }
         </div>
