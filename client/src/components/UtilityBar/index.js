6d3c9f4e7ba1e7613dfff08817b06f54044b4727
ThereWolf <bdunham484@gmail.com>
2023-04-07 16:46:19 -0500

    customize date picker

--- client/src/components/UtilityBar/index.js
+++ client/src/components/UtilityBar/index.js
@@ -10,12 +10,16 @@ const UtilityBar = () => {
 
     const [startDate, setStartDate] = useState(new Date());
 
-    console.log('CONSOLELOG: ' + startDate);
+    // console.log('CONSOLELOG: ' + startDate);
 
     const handleDateSelect = (datePick) => {
         setStartDate(datePick);
-        console.log('STARTDATE: ' + startDate);
+        // console.log('STARTDATE: ' + startDate);
         console.log('DATEPICK: ' + datePick)
+        console.log('OLDDATE: ' + date);
+        const testDate = datePick.toDateString();
+        console.log('TESTDATE: ' + testDate);
+        setDate(testDate)
     }
 
 
@@ -23,8 +27,10 @@ const UtilityBar = () => {
     const nextDayButton = (date) => {
         const next = new Date(date);
         next.setDate(next.getDate() + 1);
+        // console.log(next);
         const theNextDay = next.toDateString();
         setDate(theNextDay);
+        setStartDate(next);
     }
     //function that gets the previous day
     const dayBeforeButton = (date) => {
@@ -32,11 +38,17 @@ const UtilityBar = () => {
         before.setDate(before.getDate() - 1);
         const theLastDay = before.toDateString();
         setDate(theLastDay);
+        setStartDate(before);
+
     }
 
     return (
         <div className="utility-bar">
-            <DatePicker selected={startDate}  onSelect={(datePick) => handleDateSelect(datePick)} className={'datePicker'} />
+            {/* <DatePicker 
+                selected={startDate}  
+                onSelect={(datePick) => handleDateSelect(datePick)} className={'datePicker'}
+                dateFormat="eee MMM dd yyyy"    
+            /> */}
             <span className="display-flex date-wrapper">
                 {today === date ? (
                     <LeftArrow className="disabled-arrows" />
@@ -45,7 +57,12 @@ const UtilityBar = () => {
                 )}
 
 
-                <h3 id="date">{date}</h3>
+                {/* <h3 id="date">{date}</h3> */}
+                <DatePicker
+                    selected={startDate}
+                    onSelect={(datePick) => handleDateSelect(datePick)} className={'datePicker'}
+                    dateFormat="eee MMM dd yyyy"
+                />
                 <RightArrow className="arrows" onClick={() => nextDayButton(date)} />
             </span>
         </div>
