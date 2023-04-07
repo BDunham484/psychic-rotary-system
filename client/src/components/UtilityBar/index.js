d8a2b6224d02bdf681980f3db57e60a16791df45
ThereWolf <bdunham484@gmail.com>
2023-04-07 18:20:46 -0500

    exclude past dates

--- client/src/components/UtilityBar/index.js
+++ client/src/components/UtilityBar/index.js
@@ -2,7 +2,8 @@ import { useContext, useState } from "react";
 import { ConcertContext } from "../../utils/GlobalState";
 import { LeftArrow, RightArrow } from '@styled-icons/boxicons-regular';
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css"
+import "react-datepicker/dist/react-datepicker.css";
+import subDays from "date-fns/subDays";
 
 const UtilityBar = () => {
 
@@ -18,6 +19,12 @@ const UtilityBar = () => {
         // console.log('TESTDATE: ' + testDate);
         setDate(convertDate)
     }
+
+    const test = new Date(date);
+    test.setDate(test.getDate() - 1)
+
+    const testArr = [subDays(test, 1,), subDays(test, 2,), subDays(test, 3,), subDays(test, 4,), subDays(test, 5,), subDays(test, 6,), subDays(test, 7,), subDays(test, 8,)]
+    
 
 
     //function that gets the next day
@@ -53,6 +60,7 @@ const UtilityBar = () => {
                     onSelect={(datePick) => handleDateSelect(datePick)} className={'datePicker'}
                     calendarClassName={'calendar'}
                     dateFormat="eee MMM dd yyyy"
+                    excludeDateIntervals={[{start: subDays(test, 31), end: test}]}
                 />
                 <RightArrow className="arrows" onClick={() => nextDayButton(date)} />
             </span>
