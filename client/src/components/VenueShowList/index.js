36e66b951ca4d81a96ba7addde7626d908b6060c
ThereWolf <bdunham484@gmail.com>
2023-05-12 16:47:01 -0500

    load screen for VenueShowList

--- client/src/components/VenueShowList/index.js
+++ client/src/components/VenueShowList/index.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
 import Auth from '../../utils/auth';
 import { Link } from "react-router-dom";
 import ShowCard from "../ShowCard";
+import Spinner from '../shared/Spinner';
 import PlusMinus from "../shared/PlusMinus";
 import { SquaredPlus } from '@styled-icons/entypo/SquaredPlus';
 import BackButton from '../shared/BackButton'
 
 
 const VenueShowList = ({ concerts }) => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadData = () => {
+            setTimeout(() => {
+                setLoading(false);
+            }, 1000)
+        };
+
+        loadData();
+    }, [])
+
     const venue = concerts[0].venue
 
     if (!concerts.length) {
@@ -16,36 +30,44 @@ const VenueShowList = ({ concerts }) => {
     const loggedIn = Auth.loggedIn();
 
     return (
-        <div className='venue-show-list-wrapper'>
-            <div className="venue-heading-wrapper">
-                <BackButton />
-                <h2 className='venue-heading'>{venue}</h2>
-                <div className='spacer'></div>
-            </div>
-            {concerts &&
-                concerts.map((concert, index) => (
-                    <div key={concert._id}>
-                        <div className='venue-list-dates'>{concert.date}</div>
-                        <ShowCard>
-                            <div id="show-card-contents">
-                                <div>
-                                    {loggedIn
-                                        ? <PlusMinus concertId={concert._id} />
-                                        : <SquaredPlus id="plus-sign-logged-out" />
-                                    }
-                                </div>
-                                <div id="show-card-data">
-                                    <Link to={`/show/${concert.customId}`} state={{ concert: concert }} >
-                                        <span id="artists-link">{concert.artists} </span>
-                                    </Link>
-                                    <span id="at-venue">at</span> <span id="venue">{concert.venue}</span> <span id="divider">|</span> {concert.times}
-                                </div>
-                            </div>
-                        </ShowCard>
+        <>
+            {loading ? (
+                <div className="page-wrapper">
+                    <Spinner />
+                </div>
+            ) : (
+                <div className='venue-show-list-wrapper'>
+                    <div className="venue-heading-wrapper">
+                        <BackButton />
+                        <h2 className='venue-heading'>{venue}</h2>
+                        <div className='spacer'></div>
                     </div>
-                ))
-            }
-        </div>
+                    {concerts &&
+                        concerts.map((concert, index) => (
+                            <div key={concert._id}>
+                                <div className='venue-list-dates'>{concert.date}</div>
+                                <ShowCard>
+                                    <div id="show-card-contents">
+                                        <div>
+                                            {loggedIn
+                                                ? <PlusMinus concertId={concert._id} />
+                                                : <SquaredPlus id="plus-sign-logged-out" />
+                                            }
+                                        </div>
+                                        <div id="show-card-data">
+                                            <Link to={`/show/${concert.customId}`} state={{ concert: concert }} >
+                                                <span id="artists-link">{concert.artists} </span>
+                                            </Link>
+                                            <span id="at-venue">at</span> <span id="venue">{concert.venue}</span> <span id="divider">|</span> {concert.times}
+                                        </div>
+                                    </div>
+                                </ShowCard>
+                            </div>
+                        ))
+                    }
+                </div>
+            )}
+        </>
     )
 }
 
