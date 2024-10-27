a6fe72964da1f8d23a897e2d360a5c5f35895657
ThereWolf <bdunham484@gmail.com>
2024-10-27 08:19:43 -0500

    Update fileTypes

--- client/src/pages/NoMatch.js
+++
@@ -1,9 +0,0 @@
-const NoMatch = () => {
-    return (
-        <div className="page-wrapper">  
-            <h2>Oops, we couldn't find that page</h2>
-        </div>
-    )
-}
-
-export default NoMatch;
