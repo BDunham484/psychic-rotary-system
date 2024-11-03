b5d9fc7ea675b5297648fa30f448abf0f5907411
ThereWolf <bdunham484@gmail.com>
2024-11-03 08:18:27 -0600

    Refactor ProfileConcerts.jsx

--- client/src/components/Profile/ProfileConcerts.jsx
+++ client/src/components/Profile/ProfileConcerts.jsx
@@ -1,10 +1,9 @@
+// @ts-ignore
+import styles from './Profile.module.css';
 import { useMutation } from '@apollo/client';
 import { DELETE_CONCERT_FROM_USER } from '../../utils/mutations';
 import ShowCard from '../ShowCard/ShowCard';
 import { Link } from 'react-router-dom';
-// @ts-ignore
-import styles from './Profile.module.css';
-
 
 const ProfileConcerts = ({ userParam, user }) => {
     const {
@@ -14,22 +13,17 @@ const ProfileConcerts = ({ userParam, user }) => {
         artistsLink,
     } = styles;
 
-
-    // Delete saved concert
     const [deleteConcert] = useMutation(DELETE_CONCERT_FROM_USER);
 
-    // Function to delete concert from user
     const deleteConcertFromUser = async (id) => {
-        console.log('delete concert from user');
-        console.log(id);
         try {
             await deleteConcert({
                 variables: { concertId: id }
             });
         } catch (e) {
             console.error(e);
-        }
-    }
+        };
+    };
 
     return (
         <div>
@@ -44,7 +38,7 @@ const ProfileConcerts = ({ userParam, user }) => {
                         <div id={profileShowCardData}>
                             <div>{concert.date}</div>
                             <Link to={`/show/${concert.customId}`} state={{ concert }}>
-                                <span id={artistsLink}>{concert.artists} </span>
+                                <span id={artistsLink}>{concert.artists}</span>
                             </Link>
                             {concert.venue}
                             {!userParam &&
