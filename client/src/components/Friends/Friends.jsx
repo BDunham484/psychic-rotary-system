81ee3dccc7561a6a49ccbeeb10844be70b12236c
ThereWolf <bdunham484@gmail.com>
2024-11-09 17:23:08 -0600

    Refactor Friends.js

--- client/src/components/Friends/Friends.jsx
+++ client/src/components/Friends/Friends.jsx
@@ -1,33 +1,59 @@
-import { useState } from "react";
-import RequestBlock from "../RequestBlock/RequestBlock";
-import PendingRequests from "../PendingRequests";
-import FriendList from "../FriendList";
-import BlockedFriends from "../BlockedFriends";
+// @ts-ignore
+import styles from './styles/Friends.module.css';
+import { useState, useEffect } from "react";
+import FriendList from "./FriendList";
+import FriendSearch from "./FriendSearch";
+// import Expander from "../shared/Expander";
+import Switch from 'react-switch';
 
+const Friends = ({ user }) => {
+    const [friendSwitch, setFriendSwitch] = useState(true);
+    const [expand, setExpand] = useState(false);
+    const {
+        friendListWrapper,
+        profileFriendsSubHeader,
+        friendsSubTitles,
+        friendCount,
+        friendListIcons
+    } = styles;
 
-const Friends = ({ userParam, user }) => {
+    // switch handler to hide/display friend list options
+    const handleFriendSwitch = () => {
+        friendSwitch ? setFriendSwitch(false) : setFriendSwitch(true)
+    }
 
-    const [inputSwitched, setInputSwitched] = useState(true);
+    useEffect(() => {
+        if (user.friendCount === 0) {
+            setExpand(false);
+        }
+    }, [user])
+
+    console.log('user: ', user);
+
 
     return (
-        <div className="profile-friends-card">
-            <div className="profile-friends-card-header" >
-                <h2>Friends</h2>
-            </div>
-            <RequestBlock userParam={userParam} user={user} inputSwitched={inputSwitched} setInputSwitched={setInputSwitched} />
-            {inputSwitched &&
-                <>
-                    <PendingRequests user={user} />
-                    <FriendList user={user} />
-                </>
-
-            }
-            {!inputSwitched &&
-                <BlockedFriends user={user} />
-            }
+        <div id={`${friendListWrapper} display-flex`}>
+            {/* <div className={profileFriendsSubHeader}>
+                <div className={friendsSubTitles}>
+                    {`Total: ${user.friendCount}`}
+                </div>
+                <Switch
+                    onChange={handleFriendSwitch}
+                    checked={friendSwitch}
+                    offColor={'#525050'}
+                    onColor={'#525050'}
+                    offHandleColor={'#383737'}
+                    onHandleColor={'#383737'}
+                    uncheckedIcon={false}
+                    checkedIcon={false}
+                    boxShadow={'#eee3d0'}
+                    activeBoxShadow={'#eee3d0'}
+                />
+            </div> */}
+            <FriendSearch user={user} />
+            <FriendList user={user} />
         </div>
     )
 }
 
-export default Friends
-
+export default Friends;
