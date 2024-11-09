6bdfd935c441a255242abb1f900b97170bc922d6
ThereWolf <bdunham484@gmail.com>
2024-11-09 17:25:49 -0600

    Cleanup/deletions

--- client/src/components/RequestBlockHeader/RequestBlockHeader.jsx
+++ client/src/components/RequestBlockHeader/RequestBlockHeader.jsx
@@ -7,48 +7,46 @@ const RequestBlockHeader = ({ inputSwitched, setInputSwitched }) => {
     };
 
     return (
-        <div>
-            <div className="display-flex">
-                {inputSwitched ? (
-                    <>
-                        <h3 className='friends-sub-titles'>Request</h3>
-                        <label>
-                            <Switch
-                                onChange={handleInputSwitch}
-                                checked={inputSwitched}
-                                offColor={'#525050'}
-                                onColor={'#525050'}
-                                offHandleColor={'#383737'}
-                                onHandleColor={'#383737'}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                boxShadow={'#eee3d0'}
-                                activeBoxShadow={'#eee3d0'}
-                            />
-                        </label>
-                        <h3 className="unSwitched">Block</h3>
-                    </>
-                ) : (
-                    <>
-                        <h3 className="unSwitched">Request</h3>
-                        <label>
-                            <Switch
-                                onChange={handleInputSwitch}
-                                checked={inputSwitched}
-                                offColor={'#525050'}
-                                onColor={'#525050'}
-                                offHandleColor={'#383737'}
-                                onHandleColor={'#383737'}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                boxShadow={'#eee3d0'}
-                                activeBoxShadow={'#eee3d0'}
-                            />
-                        </label>
-                        <h3 className='friends-sub-titles'>Block</h3>
-                    </>
-                )}
-            </div>
+        <div className="display-flex">
+            {inputSwitched ? (
+                <>
+                    <h3 className='friends-sub-titles'>Request</h3>
+                    <label>
+                        <Switch
+                            onChange={handleInputSwitch}
+                            checked={inputSwitched}
+                            offColor={'#525050'}
+                            onColor={'#525050'}
+                            offHandleColor={'#383737'}
+                            onHandleColor={'#383737'}
+                            uncheckedIcon={false}
+                            checkedIcon={false}
+                            boxShadow={'#eee3d0'}
+                            activeBoxShadow={'#eee3d0'}
+                        />
+                    </label>
+                    <h3 className="unSwitched">Block</h3>
+                </>
+            ) : (
+                <>
+                    <h3 className="unSwitched">Request</h3>
+                    <label>
+                        <Switch
+                            onChange={handleInputSwitch}
+                            checked={inputSwitched}
+                            offColor={'#525050'}
+                            onColor={'#525050'}
+                            offHandleColor={'#383737'}
+                            onHandleColor={'#383737'}
+                            uncheckedIcon={false}
+                            checkedIcon={false}
+                            boxShadow={'#eee3d0'}
+                            activeBoxShadow={'#eee3d0'}
+                        />
+                    </label>
+                    <h3 className='friends-sub-titles'>Block</h3>
+                </>
+            )}
         </div>
     )
 }
