f0db0f4d71dfbbab607d734e482480b11a78bb2d
ThereWolf <bdunham484@gmail.com>
2024-11-10 06:32:45 -0600

    Add skeleton list items

--- client/src/components/Friends/FriendList.jsx
+++ client/src/components/Friends/FriendList.jsx
@@ -3,7 +3,8 @@ import styles from './styles/Friends.module.css';
 import { Link } from "react-router-dom";
 import FriendListOptions from "./FriendListOptions";
 import Switch from 'react-switch';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import SkeletonFriendListItem from './SkeletonFriendListItem';
 
 const FriendList = ({ user }) => {
     const [friendSwitch, setFriendSwitch] = useState(true);
@@ -16,7 +17,21 @@ const FriendList = ({ user }) => {
         friendListOptionsWrapper,
     } = styles;
 
-    let usersFriends = [...user.friends];
+    const arrayLength = useMemo(() => {
+        if (user?.friendCount < 8) {
+            return 8 - user?.friendCount
+        } else {
+            return 0;
+        }
+    }, [user?.friendCount]);
+
+    const subArray = new Array(arrayLength);
+
+    let usersFriends = [
+        ...user.friends,
+        ...subArray,
+    ];
+
     // Sort 
     usersFriends.sort(function (a, b) {
         if (a.username < b.username) {
@@ -37,30 +52,44 @@ const FriendList = ({ user }) => {
         <div>
             <div className={friendListContainer}>
                 <div className={user.friendCount > 8 ? `${friendListScroll} ${styledScrollbars}` : ''}>
-                    {usersFriends.map(friend => (
-                        <>
-                            <div key={friend._id} className={names}>
-                                <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
-                                <Switch
-                                    onChange={() => handleFriendSwitch(friend._id)}
-                                    checked={!friendSwitch && friendSwitchId === friend._id}
-                                    offColor={'#525050'}
-                                    onColor={'#525050'}
-                                    offHandleColor={'#383737'}
-                                    onHandleColor={'#383737'}
-                                    uncheckedIcon={false}
-                                    checkedIcon={false}
-                                    boxShadow={'#eee3d0'}
-                                    activeBoxShadow={'#eee3d0'}
-                                />
-                            </div>
-                            {(!friendSwitch && friendSwitchId === friend._id) &&
-                                <div className={friendListOptionsWrapper}>
-                                    <FriendListOptions friendId={friend._id} />
+                    {usersFriends.map((friend, index) => {
+                        if (!!friend?._id) {
+                            return (
+                                <>
+                                    <div key={friend._id} className={names}>
+                                        <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
+                                        <Switch
+                                            onChange={() => handleFriendSwitch(friend._id)}
+                                            checked={!friendSwitch && friendSwitchId === friend._id}
+                                            offColor={'#525050'}
+                                            onColor={'#525050'}
+                                            offHandleColor={'#383737'}
+                                            onHandleColor={'#383737'}
+                                            uncheckedIcon={false}
+                                            checkedIcon={false}
+                                            boxShadow={'#eee3d0'}
+                                            activeBoxShadow={'#eee3d0'}
+                                        />
+                                    </div>
+                                    {(!friendSwitch && friendSwitchId === friend._id) &&
+                                        <div className={friendListOptionsWrapper}>
+                                            <FriendListOptions friendId={friend._id} />
+                                        </div>
+                                    }
+                                </>
+                            )
+                        } else {
+                            return (
+                                <div key={index} style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}>
+                                    <SkeletonFriendListItem />
                                 </div>
-                            }
-                        </>
-                    ))}
+                            )
+                        }
+                    })}
                 </div>
             </div>
         </div>
