84265ae6266b0d7958b9fc3af8786ee1c11457ea
ThereWolf <bdunham484@gmail.com>
2024-11-10 14:09:49 -0600

    Media Queries/responsiveness

--- client/src/components/Friends/ProfileFriends.jsx
+++ client/src/components/Friends/ProfileFriends.jsx
@@ -1,31 +1,86 @@
 // @ts-ignore
 import styles from './styles/Friends.module.css';
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import RequestBlock from "../RequestBlock/RequestBlock";
 import PendingRequests from "./PendingRequests";
 import Friends from './Friends';
 import BlockedFriends from "./BlockedFriends";
+import Tabs from '../shared/Tabs/Tabs';
+import { User } from '@styled-icons/fa-solid/User';
+import { UserPlus } from '@styled-icons/fa-solid/UserPlus';
+import { UserXmark } from '@styled-icons/fa-solid/UserXmark';
+import RequestBlockForm from '../RequestBlockForm';
+
 
 const ProfileFriends = ({ userParam, user }) => {
     const [inputSwitched, setInputSwitched] = useState(true);
     const {
         friendsWrapper,
         requestBlockWrapper,
+        tabButton,
+        activeListTab,
+        activeRequestTab,
+        activeBlockTab,
+        tabContent,
+        tabWrapper,
     } = styles;
 
-    return (
-        <div id={friendsWrapper}>
-            <Friends user={user} />
-            <div id={requestBlockWrapper}>
-                <RequestBlock userParam={userParam} user={user} inputSwitched={inputSwitched} setInputSwitched={setInputSwitched} />
-                {inputSwitched &&
+    const customTabStyles = useMemo(() => {
+        return {
+            customTabButton: tabButton,
+            activeListTab: activeListTab,
+            activeRequestTab: activeRequestTab,
+            activeBlockTab: activeBlockTab,
+            customTabContent: tabContent,
+        }
+    }, [tabButton, activeListTab, activeRequestTab, activeBlockTab, tabContent]);
+
+    const tabData = [
+        {
+            label: <User />,
+            content:
+                <div>
+                    {/* <Friends user={user} customStyles={}/> */}
+                    <Friends user={user} />
+                </div>
+        },
+        {
+            label: <UserPlus />,
+            content:
+                <div>
+                    <RequestBlockForm userParam={userParam} inputSwitched={true} user={user} />
                     <PendingRequests user={user} />
-                }
-                {!inputSwitched &&
+                </div>
+        },
+        {
+            label: <UserXmark />,
+            content:
+                <div>
+                    <RequestBlockForm userParam={userParam} inputSwitched={false} user={user} />
                     <BlockedFriends user={user} />
-                }
+                </div>
+        },
+    ];
+
+    return (
+        <>
+            <div className={tabWrapper}>
+                <Tabs tabs={tabData} customStyles={customTabStyles} />
             </div>
-        </div>
+            <div id={friendsWrapper}>
+                {/* <Tabs tabs={tabData} customStyles={customTabStyles}/> */}
+                <Friends user={user} />
+                <div id={requestBlockWrapper}>
+                    <RequestBlock userParam={userParam} user={user} inputSwitched={inputSwitched} setInputSwitched={setInputSwitched} />
+                    {inputSwitched &&
+                        <PendingRequests user={user} />
+                    }
+                    {!inputSwitched &&
+                        <BlockedFriends user={user} />
+                    }
+                </div>
+            </div>
+        </>
     )
 }
 
--- client/src/components/shared/Tabs/Tabs.jsx
+++ client/src/components/shared/Tabs/Tabs.jsx
@@ -2,12 +2,42 @@ import { useEffect, useRef, useState } from 'react';
 // @ts-ignore
 import styles from './Tabs.module.css';
 
-const Tabs = ({ tabs }) => {
+const Tabs = ({
+    tabs,
+    customStyles = {
+        customTabButton: undefined,
+        activeListTab: undefined,
+        activeRequestTab: undefined,
+        activeBlockTab: undefined,
+        customTabContent: undefined,
+    },
+}) => {
+    // const [activeTab, setActiveTab] = useState(1);
     const [activeTab, setActiveTab] = useState(0);
     const [fade, setFade] = useState(false);
     const [slideDirection, setSlideDirection] = useState('');
     const [sliderStyle, setSliderStyle] = useState({});
     const tabRefs = useRef([]); // Store references to each tab button
+    const {
+        tabsContainer,
+        tabs: tabsStyles,
+        active,
+        tabButton,
+        slider,
+        tabContent,
+        fadeOut,
+        fadeIn,
+    } = styles;
+    const [tabContentStyles, setTabContentStyles] = useState(tabContent);
+    const [buttonStyles, setButtonStyles] = useState(tabButton);
+    // const [activeTabState, setActiveTabState] = useState(0);
+    const {
+        customTabButton,
+        activeListTab,
+        activeRequestTab,
+        activeBlockTab,
+        customTabContent,
+    } = customStyles;
 
     useEffect(() => {
         // Position the slider based on the active tab
@@ -19,6 +49,23 @@ const Tabs = ({ tabs }) => {
             });
         }
     }, [activeTab]);
+
+    useEffect(() => {
+        if (customTabButton) {
+            setButtonStyles(customTabButton);
+        }
+
+        if (customTabContent) {
+            setTabContentStyles(customTabContent);
+        }
+
+        // if ((tabs.label?.includes('Friends'))) {
+        //     console.log('👾👾👾👾 activeTab: ', activeTab);
+        //     console.log('👾👾👾👾 (tabs.label.includes(Friends)): ', (tabs.label.includes('Friends')));
+        //     console.log(' ');
+        //     setTabContentStyles(customTabContent)
+        // }
+    }, [customTabButton, customTabContent]);
 
     const handleTabClick = (index) => {
         if (index !== activeTab) {
@@ -32,23 +79,38 @@ const Tabs = ({ tabs }) => {
     };
 
     return (
-        <div className={styles.tabsContainer}>
-            <div className={styles.tabs}>
-                {tabs.map((tab, index) => (
-                    <button
-                        key={index}
-                        ref={(el) => (tabRefs.current[index] = el)} // Assign ref to each tab button
-                        className={`${styles.tabButton} ${activeTab === index ? styles.active : ''}`}
-                        onClick={() => handleTabClick(index)}
-                    >
-                        <h2>{tab.label}</h2>
-                    </button>
-                ))}
-                <div className={styles.slider} style={sliderStyle}></div>
+        <div className={tabsContainer}>
+            <div className={tabsStyles}>
+                {tabs.map((tab, index) => {
+                    let testStyles = active;
+                    if (activeListTab && (activeTab === 0)) {
+                        testStyles = activeListTab;
+                    } else if (activeRequestTab && (activeTab === 1)) {
+                        testStyles = activeRequestTab;
+                    }
+                    if (activeBlockTab && (activeTab === 2)) {
+                        testStyles = activeBlockTab;
+                    }
+
+                    return (
+                        <button
+                            key={index}
+                            ref={(el) => (tabRefs.current[index] = el)} // Assign ref to each tab button
+                            // changelog-start
+                            className={`${buttonStyles} ${activeTab === index ? testStyles : ''}`}
+                            // className={`${styles.tabButton} ${activeTab === index ? styles.active : ''}`}
+                            // changelog-end
+                            onClick={() => handleTabClick(index)}
+                        >
+                            <h2>{tab.label}</h2>
+                        </button>
+                    )
+                })}
+                <div className={slider} style={sliderStyle}></div>
             </div>
             <div className={`
-                ${styles.tabContent}
-                ${fade ? styles.fadeOut : styles.fadeIn}
+                ${tabContentStyles}
+                ${fade ? fadeOut : fadeIn}
                 ${styles[slideDirection]}
                 `}
                 onAnimationEnd={() => setSlideDirection('')} // Reset slide direction after animation
