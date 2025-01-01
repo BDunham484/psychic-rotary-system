05416360bb9f99528d3956f2e774ccdc4bd70b13
ThereWolf <bdunham484@gmail.com>
2024-12-31 18:27:59 -0600

    Conditionaly display show data

--- client/src/pages/Show.jsx
+++ client/src/pages/Show.jsx
@@ -8,16 +8,14 @@ import { SquaredPlus } from '@styled-icons/entypo/SquaredPlus';
 import DisabledConcertRSVP from "../components/DisabledConcertRSVP";
 
 const Show = () => {
-
     const location = useLocation();
-
     const { concert } = location.state
 
-    const googleMaps = `https://www.google.com/maps/search/?api=1&query=${concert.venue}`
+    const googleMaps = `https://www.google.com/maps/search/?api=1&query=${concert?.venue}`
 
-    const wazeMaps = `https://waze.com/ul?q=${concert.venue}&navigate=yes`;
+    const wazeMaps = `https://waze.com/ul?q=${concert?.venue}&navigate=yes`;
 
-    const phoneNumber = `tel:${concert.phone}`;
+    const phoneNumber = `tel:${concert?.phone}`;
 
     const loggedIn = Auth.loggedIn();
 
@@ -37,32 +35,50 @@ const Show = () => {
             <ShowCard>
                 <div className="show-wrapper">
                     <h2>
-                        {concert.artists}
+                        {concert?.artists}
                     </h2>
-                    <h3>at
-                        <a href={concert.website}> {concert.venue}</a> | {concert.times}</h3>
-
+                    {concert?.venue &&
+                        <h3>
+                            at <a href={concert.website}> {concert.venue}</a>
+                        </h3>
+                    }
                     <ul className="show-links">
-                        <li>
-                            {concert.address}
-                        </li>
-                        {concert.address2 &&
+                        {concert?.times &&
+                            <li>
+                                {concert.times}
+                            </li>
+                        }
+                        {concert?.address &&
+                            <li>
+                                {concert.address}
+                            </li>
+                        }
+                        {concert?.address2 &&
                             <li id="address2">
                                 {concert.address2}
-                            </li>}
-                        <li>
-                            <a href={phoneNumber}>{concert.phone}</a>
-                        </li>
-                        <li>
-                            <a href={googleMaps}>Open in Google Maps</a>
-                        </li>
-                        <li>
-                            <a href={wazeMaps}>Open in Waze</a>
-                        </li>
-                        <li>
-                            <a href={"mailto:" + concert.email}>{concert.email}</a>
-                        </li>
-                        {concert.ticketLink &&
+                            </li>
+                        }
+                        {concert?.phone &&
+                            <li>
+                                <a href={phoneNumber}>{concert.phone}</a>
+                            </li>
+                        }
+                        {concert?.venue &&
+                            <>
+                                <li>
+                                    <a href={googleMaps}>Open in Google Maps</a>
+                                </li>
+                                <li>
+                                    <a href={wazeMaps}>Open in Waze</a>
+                                </li>
+                            </>
+                        }
+                        {concert?.email &&
+                            <li>
+                                <a href={"mailto:" + concert.email}>{concert.email}</a>
+                            </li>
+                        }
+                        {concert?.ticketLink &&
                             <li>
                                 <a href={concert.ticketLink}>Get Tickets</a>
                             </li>
