3c9538aed59cb007d987c47e51c6878b7f11d1ba
thereWolf <bdunham484@gmail.com>
2026-05-17 14:20:37 -0500

    feat: rewrite Home page with DateNav + ControlBar + ConcertList

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,41 +1,76 @@
-import { useContext, useState } from "react";
-import ConcertsVenueAZ from '../components/ConcertsVenueAZ/ConcertsVenueAZ';
-import ConcertsArtistsAZ from '../components/ConcertsArtistsAZ/ConcertsArtistsAZ';
-import { ConcertContext } from '../utils/GlobalState'
-import UtilityBar from '../components/UtilityBar/UtilityBar';
-import SortFilterBar from '../components/SortFilterBar/SortFilterBar';
-import VenueSearch from "../components/VenueSearch/VenueSearch";
+import { useContext, useMemo, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { ConcertContext } from '../utils/GlobalState';
+import {
+  GET_CONCERTS_SORTED_BY_VENUE_ASC,
+  GET_CONCERTS_SORTED_BY_VENUE_DESC,
+  GET_CONCERTS_SORTED_BY_ARTISTS_ASC,
+  GET_CONCERTS_SORTED_BY_ARTISTS_DESC,
+} from '../utils/queries';
+import DateNav from '../components/DateNav/DateNav';
+import ControlBar from '../components/ControlBar/ControlBar';
+import ConcertList from '../components/ConcertList/ConcertList';
+import VenueSearch from '../components/VenueSearch/VenueSearch';
+import ScrollButton from '../components/shared/ScrollButton';
+import styles from './Home.module.css';
 
+const queryMap = {
+  'venue-asc':   GET_CONCERTS_SORTED_BY_VENUE_ASC,
+  'venue-desc':  GET_CONCERTS_SORTED_BY_VENUE_DESC,
+  'artist-asc':  GET_CONCERTS_SORTED_BY_ARTISTS_ASC,
+  'artist-desc': GET_CONCERTS_SORTED_BY_ARTISTS_DESC,
+};
 
 const Home = () => {
-  const [optionsOpen, setOptionsOpen] = useState(false);
+  const { date, setDate, sortOrSearch, setSortOrSearch } = useContext(ConcertContext);
   const [isAsc, setIsAsc] = useState(true);
 
-  const { date, sortOrSearch, setSortOrSearch  } = useContext(ConcertContext);
+  const isSearchMode = sortOrSearch === 'search';
+  const key = `${sortOrSearch}-${isAsc ? 'asc' : 'desc'}`;
+  const activeQuery = queryMap[key] || GET_CONCERTS_SORTED_BY_VENUE_ASC;
+
+  const { loading, data } = useQuery(activeQuery, {
+    variables: { date },
+    skip: isSearchMode,
+  });
+
+  const concerts = useMemo(() => {
+    if (!data) return [];
+    return data.concertsSortByVenueAsc
+        || data.concertsSortByVenueDesc
+        || data.concertsSortByArtistsAsc
+        || data.concertsSortByArtistsDesc
+        || [];
+  }, [data]);
+
+  const handleSort = (mode) => {
+    if (mode === sortOrSearch) {
+      setIsAsc(prev => !prev);
+    } else {
+      setSortOrSearch(mode);
+      setIsAsc(true);
+    }
+  };
 
   return (
-    <div id='home-wrapper'>
-      <UtilityBar optionsOpen={optionsOpen} setOptionsOpen={setOptionsOpen} />
-      {optionsOpen &&
-        <SortFilterBar setSortOrSearch={setSortOrSearch} setIsAsc={setIsAsc} isAsc={isAsc}/>
-      }
-      <div className={optionsOpen ? 'wrapperOptions' : 'wrapper'}>
-        <div className={`home-page-wrapper`}>
-          
-            <div>
-              {sortOrSearch === 'venue' &&
-                <ConcertsVenueAZ date={date} isAsc={isAsc} />
-              }
-              {sortOrSearch === 'artist' &&
-                <ConcertsArtistsAZ date={date} isAsc={isAsc} />
-              }
-              {sortOrSearch === 'search' &&
-                <VenueSearch />
-              }
-            </div>
-        </div>
+    <main className={styles.main}>
+      <div className={styles.page}>
+        <DateNav date={date} setDate={setDate} total={isSearchMode ? 0 : concerts.length} />
+        <ControlBar
+          mode={sortOrSearch}
+          isAsc={isAsc}
+          onSort={handleSort}
+          count={isSearchMode ? 0 : concerts.length}
+        />
+        {isSearchMode
+          ? <VenueSearch />
+          : loading
+            ? null
+            : <ConcertList concerts={concerts} />
+        }
+        <ScrollButton />
       </div>
-    </div>
+    </main>
   );
 };
 
